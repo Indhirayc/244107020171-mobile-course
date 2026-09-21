d38437d5ee748395b624580f74e485402969bee2
Indhira Yuantika Christy <indhirayuantikac1176@gmail.com>
2026-09-21 22:11:42 +0700

    feat: complete week 3 AsyncValue loading, error, and success states

--- 03-week-3-navigation-state-management/week3_todo/lib/main.dart
+++ 03-week-3-navigation-state-management/week3_todo/lib/main.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'pages/todo_page.dart';
+import 'pages/product_page.dart';
 
-void main() => runApp(const ProviderScope(child: MyApp()));
+void main() => runApp(
+  ProviderScope(
+    retry: (retryCount, error) => null, // nonaktifkan auto retry
+    child: const MyApp(),
+  ),
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) => MaterialApp(
-        title: 'Week 3 - ToDo',
-        theme: ThemeData(colorSchemeSeed: Colors.teal, useMaterial3: true),
-        home: const TodoPage(),
+        title: 'Week 3 - AsyncValue',
+        theme: ThemeData(
+          colorSchemeSeed: Colors.teal,
+          useMaterial3: true,
+        ),
+        home: const ProductPage(),
       );
 }
